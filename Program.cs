198327a244f2139ecdfeb1c01e0ac0366c988c6c
user1hunter <user1hunter@gmail.com>
2022-02-28 13:18:13 -0600

    Phase 5 Done

--- Program.cs
+++ Program.cs
@@ -1,17 +1,37 @@
 ﻿using System;
 
-Console.WriteLine("Please guess the Secret Number.");
+int secretNumber = new Random().Next(1, 100);
 
-int guess = int.Parse(Console.ReadLine());
+int tries = 4;
 
-int secretNumber = 42;
+int guessNumber = 1;
 
-if (guess == secretNumber)
+while (guessNumber <= tries)
 {
-  Console.WriteLine("You guessed right!!");
-}
-else
-{
-  Console.WriteLine("Sorry, you guessed wrong.");
+  Console.WriteLine($"You're on guess {guessNumber} out of 4.");
+
+  Console.WriteLine("Please guess the Secret Number.");
+
+  int guess = int.Parse(Console.ReadLine());
+
+  if (guess == secretNumber)
+  {
+    Console.WriteLine("You guessed right!!");
+    break;
+  }
+  else
+  {
+    Console.WriteLine("Sorry, you guessed wrong.");
+
+    if (tries - guessNumber == 1)
+    {
+      Console.WriteLine("You have 1 guess remaining.");
+    }
+    else
+    {
+      Console.WriteLine($"You have {tries - guessNumber} guesses remaining.");
+    }
+  }
+  guessNumber++;
 }
 
